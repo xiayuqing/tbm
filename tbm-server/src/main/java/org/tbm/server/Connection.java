bd9c40547246bed2304d0e67669d3c68da1c741f
yuqing.xia <xiayuqing123@hotmail.com>
2017-06-30 17:27:24 +0800

    update

---
+++ tbm-server/src/main/java/org/tbm/server/Connection.java
@@ -0,0 +1,8 @@
+package org.tbm.server;
+
+/**
+ * Created by Jason.Xia on 17/6/30.
+ */
+public interface Connection {
+
+}
--- tbm-server/src/main/java/org/tbm/server/processor/HandshakeProcessor.java
+++ tbm-server/src/main/java/org/tbm/server/processor/HandshakeProcessor.java
@@ -36,15 +36,13 @@ public class HandshakeProcessor implements Processor {
             return PacketLite.createException("payload format illegal", packetLite.seq);
         }
 
-
         List<MachineBinding> select;
         try {
             select = machineBindingOp.SELECT_MACHINE_BINDING(machineInfo.getSystemId(), machineInfo.getIp());
             if (null != select && 1 < select.size()) {
                 logger.error("duplicate host info with :{}", machineInfo);
                 return PacketLite.createException("duplicate host info with system:" + machineInfo.getSystemId() + "," +
-                        "ip:" +
-                        machineInfo.getIp() + ",port:" + machineInfo.getPort(), packetLite.seq);
+                        "ip:" + machineInfo.getIp() + ",port:" + machineInfo.getPort(), packetLite.seq);
             }
 
             if (null == select || 0 == select.size()) { // 新系统上线/换机器需要重新添加绑定信息
